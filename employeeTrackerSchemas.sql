7ae5015c0ddea5edde4f267df603d05d2cb004bb
Jonathan Rein <jrein1296@gmail.com>
2021-05-03 15:44:07 -0400

    Renaming enployeeTracherSeeds to enployeTrackerSchema to more accuratly describe the file.

--- employeeTrackerSchemas.sql
+++ employeeTrackerSchemas.sql
@@ -3,19 +3,25 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE employees(
+    id INTEGER AUTO_INCREMENT NOT NULL,
     first_name varchar(30)
     last_name varchar(30)
     role_id int
     manager_id int
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE departments (
+    id INTEGER AUTO_INCREMENT NOT NULL,
     name varchar(30)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT NOT NULL,
     title varchar(30)
     salary decimal
     department_id int
+    PRIMARY KEY (id)
 );
 
