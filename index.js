33199d848a85a7bcbbce8c638580f26605322b17
Jonathan Rein <jrein1296@gmail.com>
2021-05-04 16:44:26 -0400

    Function append role roles.push comes back undefined.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
-const { userOptions, addEmployee } = require("./questions.js");
+const { userOptions, addEmployee, addRole, roles } = require("./questions.js");
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -17,32 +17,45 @@ function getUserOption() {
   inquirer.prompt(userOptions).then((data) => {
     if (data.userOption === "Add Employee") {
       insertEmployeeData();
-    } else if (data.userOption === "Add Role") {
-      appendRole()
+    } else if (data.userOption === "Add new Role") {
+      appendRole();
     }
   });
 }
 
 function insertEmployeeData() {
-  inquirer.prompt(addEmployee).then((data) => {
-    connection.query(
-      "INSERT INTO employees SET ?",
-      {
-        first_name: data.employeeFirstName,
-        last_name: data.employeeLastName,
-        role_name: data.employeeRole,
-        manager_name: data.employeeManager,
-      },
-      (err, res) => {
-        if (err) throw err;
-        console.log(`${res.affectedRows} new employee added to the database.\n`);
-      }
-    );
-  }) .then(() => getUserOption());
+  inquirer
+    .prompt(addEmployee)
+    .then((data) => {
+      connection.query(
+        "INSERT INTO employees SET ?",
+        {
+          first_name: data.employeeFirstName,
+          last_name: data.employeeLastName,
+          role_name: data.employeeRole,
+          manager_name: data.employeeManager,
+        },
+        (err, res) => {
+          if (err) throw err;
+          console.log(
+            `${res.affectedRows} new employee added to the database.\n`
+          );
+        }
+      );
+    })
+    .then(() => getUserOption());
 }
 
-// function appendRole() {
-//   inquirer.prompt(addRole).then((data) => {
-
-//   })
-// }
+function appendRole() {
+  inquirer
+    .prompt(addRole)
+    .then((data) => {
+      console.log(data.newRole);
+      roles.push(data.newRole),
+        (err, res) => {
+          if (err) throw err;
+          console.log(`Role ${res.newRole} added`);
+        };
+    })
+    .then(() => getUserOption());
+}
