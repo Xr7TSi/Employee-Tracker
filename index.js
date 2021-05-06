81a992b27339d9a99794a49ca0de67d58ebe6d9e
Jonathan Rein <jrein1296@gmail.com>
2021-05-06 11:42:26 -0400

    GetManagerArray and getRolesArray now work.  Inquirer can't access them yet.

--- index.js
+++ index.js
@@ -9,113 +9,111 @@ const connection = mysql.createConnection({
   database: "employeeTracker_db",
 });
 
-// getUserOption();
+getUserOption();
 
-// function getUserOption() {
-//   inquirer
-//     .prompt({
-//       type: "list",
-//       name: "userOption",
-//       choices: [
-//         "View all Employees",
-//         "View all Employees by Department",
-//         "View all employees by Manager",
-//         "Add new Employee",
-//         "Remove Employee",
-//         "Update Employee Role",
-//         "Update Employee Manager",
-//         "Add new Role",
-//         "Add new Department",
-//         "Quit",
-//       ],
-//     })
+function getUserOption() {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "userOption",
+      choices: [
+        "View all Employees",
+        "View all Employees by Department",
+        "View all employees by Manager",
+        "Add new Employee",
+        "Remove Employee",
+        "Update Employee Role",
+        "Update Employee Manager",
+        "Add new Role",
+        "Add new Department",
+        "Quit",
+      ],
+    })
 
-//     .then((data) => {
-//       if (data.userOption === "Add new Employee") {
-//         getRolesArray();
-//         getManagerArray();
-//         // insertEmployeeData();
-//       } else if (data.userOption === "Add new Role") {
-//         insertRoleData();
-//       } else if (data.userOption === "Add new Department") {
-//         insertDepartment();
-//       }
-//     });
-// }
+    .then((data) => {
+      if (data.userOption === "Add new Employee") {
+        getRolesArray();
+        getManagerArray();
+        // insertEmployeeData();
+      } else if (data.userOption === "Add new Role") {
+        insertRoleData();
+      } else if (data.userOption === "Add new Department") {
+        insertDepartment();
+      }
+    });
+}
 
-// let roleChoices = [];
-// let managerChoices = [];
+let roleChoices = [];
+let managerChoices = [];
 
-// function getRolesArray() {
-//   connection.query("SELECT title FROM roles", (err, res) => {
-//     if (err) throw err;
-//     roleArray = res;
-//     // console.log(roleArray);
-//     roleChoices = roleArray.map((choice) => choice.title);
-//     console.log(roleChoices);
-//     return roleChoices;
-//   });
-// }
+function getRolesArray() {
+  connection.query("SELECT title FROM roles", (err, res) => {
+    if (err) throw err;
+    roleArray = res;
+    roleChoices = roleArray.map((choice) => choice.title);
+    return roleChoices;
+  });
+}
 
 function getManagerArray() {
   connection.query("SELECT * FROM employees", (err, res) => {
     if (err) throw err;
     employeesArray = res;
-
-
-    // console.log(employeesArray);
-    managerChoices = employeesArray.filter(manager => manager.role_id === 1 || 3 || 7)
-    console.log(managerChoices);
+    managerObjects = employeesArray.filter(
+      (manager) =>
+        (manager.role_id === manager.role_id) === 1 ||
+        manager.role_id === 3 ||
+        manager.role_id === 7
+    );
+    managerChoices = managerObjects.map((choice) => choice.last_name);
     return managerChoices;
   });
-} 
+}
 
-getManagerArray()
-
-// function insertEmployeeData() {
-//   inquirer
-//     .prompt([
-//       {
-//         type: "input",
-//         message: "Enter Employee first name:",
-//         name: "employeeFirstName",
-//       },
-//       {
-//         type: "input",
-//         message: "Enter Employee last name:",
-//         name: "employeeLastName",
-//       },
-//       {
-//         type: "list",
-//         message: "Choose new employee's role:",
-//         name: "employeeRole",
-//         choices: roleChoices,
-//       },
-//       {
-//         type: "list",
-//         message: "Choose new employee's Manager:",
-//         name: "employeeManager",
-//         choices: managerChoices,
-//       },
-//     ])
-//     .then((data) => {
-//       connection.query(
-//         "INSERT INTO employees SET ?",
-//         {
-//           first_name: data.employeeFirstName,
-//           last_name: data.employeeLastName,
-//           role_id: data.employeeRole,
-//           manager_id: data.employeeManager,
-//         },
-//         (err, res) => {
-//           if (err) throw err;
-//           console.log(
-//             `${res.affectedRows} new employee added to the database.\n`
-//           );
-//         }
-//       );
-//     });
-// }
+function insertEmployeeData() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter Employee first name:",
+        name: "employeeFirstName",
+      },
+      {
+        type: "input",
+        message: "Enter Employee last name:",
+        name: "employeeLastName",
+      },
+      {
+        type: "list",
+        message: "Choose new employee's role:",
+        name: "employeeRole",
+        choices: roleChoices,
+      },
+      {
+        type: "list",
+        message: "Choose new employee's Manager:",
+        name: "employeeManager",
+        choices: managerChoices,
+      },
+    ])
+    .then((data) => {
+      connection.query(
+        "INSERT INTO employees SET ?",
+        {
+          first_name: data.employeeFirstName,
+          last_name: data.employeeLastName,
+          role_id: data.employeeRole,
+          manager_id: data.employeeManager,
+        },
+        (err, res) => {
+          if (err) throw err;
+          console.log(
+            `${res.affectedRows} new employee added to the database.\n`
+          );
+        }
+      );
+    });
+}
 
 // function insertRoleData() {
 //   inquirer
