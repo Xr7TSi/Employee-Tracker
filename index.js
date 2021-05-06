baa27046e8c89fefe29d4f1278b1d16cc2e2eac1
Jonathan Rein <jrein1296@gmail.com>
2021-05-06 13:52:37 -0400

    Push before AskBCS support.

--- index.js
+++ index.js
@@ -9,165 +9,195 @@ const connection = mysql.createConnection({
   database: "employeeTracker_db",
 });
 
+// let roleChoices = [];
 
-let roleChoices = [];
+// function getRolesArray() {
+//   connection.query("SELECT title FROM roles", (err, res) => {
+//     if (err) throw err;
+//     roleArray = res;
+//     roleChoices = roleArray.map((choice) => choice.title);
+//     return roleChoices;
+//   });
+// }
+// getRolesArray();
 
-function getRolesArray() {
-  connection.query("SELECT title FROM roles", (err, res) => {
-    if (err) throw err;
-    roleArray = res;
-    roleChoices = roleArray.map((choice) => choice.title);
-    return roleChoices;
-  });
-}
-getRolesArray();
+// let roleChoices = [];
 
+// function getRolesArray() {
+//   connection.query("SELECT * FROM roles", (err, res) => {
+//     if (err) throw err;
+//     console.log(res)
 
-let managerChoices = [];
+//     const roleChoices = [{ name: "This is option 1", value: 1}, { name: "This is option 1", value: 1} ]
+//     return roleChoices;
+//   });
+// }
+// getRolesArray();
 
-function getManagerArray() {
+// let managerChoices = [];
+
+// function getManagersArray() {
+//   connection.query("SELECT * FROM employees", (err, res) => {
+//     if (err) throw err;
+//     employeesArray = res;
+//     managerObjects = employeesArray.filter(
+//       (manager) =>
+//         (manager.role_id === manager.role_id) === 1 ||
+//         manager.role_id === 3 ||
+//         manager.role_id === 7
+//     );
+//     managerChoices = managerObjects.map((choice) => choice.last_name);
+//     return managerChoices;
+//   });
+// }
+// getManagersArray();
+
+// let managerChoices = [];
+
+function getManagersArray() {
   connection.query("SELECT * FROM employees", (err, res) => {
     if (err) throw err;
     employeesArray = res;
+    // console.log(employeesArray)
     managerObjects = employeesArray.filter(
       (manager) =>
-        (manager.role_id === manager.role_id) === 1 ||
-        manager.role_id === 3 ||
-        manager.role_id === 7
+        manager.role_id === 1 || manager.role_id === 3 || manager.role_id === 7
     );
-    managerChoices = managerObjects.map((choice) => choice.last_name);
+    console.log(managerObjects);
+
+    const managerChoices = [
+      
+    ];
     return managerChoices;
   });
 }
-getManagerArray();
+getManagersArray();
 
-getUserOption();
+// getUserOption();
 
-function getUserOption() {
-  inquirer
-    .prompt({
-      type: "list",
-      name: "userOption",
-      choices: [
-        "View all Employees",
-        "View all Employees by Department",
-        "View all employees by Manager",
-        "Add new Employee",
-        "Remove Employee",
-        "Update Employee Role",
-        "Update Employee Manager",
-        "Add new Role",
-        "Add new Department",
-        "Quit",
-      ],
-    })
+// function getUserOption() {
+//   inquirer
+//     .prompt({
+//       type: "list",
+//       name: "userOption",
+//       choices: [
+//         "View all Employees",
+//         "View all Employees by Department",
+//         "View all employees by Manager",
+//         "Add new Employee",
+//         "Remove Employee",
+//         "Update Employee Role",
+//         "Update Employee Manager",
+//         "Add new Role",
+//         "Add new Department",
+//         "Quit",
+//       ],
+//     })
 
-    .then((data) => {
-      if (data.userOption === "Add new Employee") {
-        insertEmployeeData();
-      } else if (data.userOption === "Add new Role") {
-        insertRoleData();
-      } else if (data.userOption === "Add new Department") {
-        insertDepartment();
-      }
-    });
-}
+//     .then((data) => {
+//       if (data.userOption === "Add new Employee") {
+//         insertEmployeeData();
+//       } else if (data.userOption === "Add new Role") {
+//         insertRoleData();
+//       } else if (data.userOption === "Add new Department") {
+//         insertDepartment();
+//       }
+//     });
+// }
 
+// function insertEmployeeData() {
+//   inquirer
+//     .prompt([
+//       {
+//         type: "input",
+//         message: "Enter Employee first name:",
+//         name: "employeeFirstName",
+//       },
+//       {
+//         type: "input",
+//         message: "Enter Employee last name:",
+//         name: "employeeLastName",
+//       },
+//       {
+//         type: "list",
+//         message: "Choose new employee's role:",
+//         name: "employeeRole",
+//         choices: roleChoices,
+//       },
+//       {
+//         type: "list",
+//         message: "Choose new employee's Manager:",
+//         name: "employeeManager",
+//         choices: managerChoices,
+//       },
+//     ])
+//     .then((data) => {
+//       connection.query(
+//         "INSERT INTO employees SET ?",
+//         {
+//           first_name: data.employeeFirstName,
+//           last_name: data.employeeLastName,
+//           role_id: data.employeeRole,
+//           manager_id: data.employeeManager,
+//         },
+//         (err, res) => {
+//           if (err) throw err;
+//           console.log(
+//             `${res.affectedRows} new employee added to the database.\n`
+//           );
+//         }
+//       );
+//     });
+// }
 
+// function insertRoleData() {
+//   inquirer
+//     .prompt([
+//       {
+//         type: "input",
+//         message: "Enter Role Title:",
+//         name: "roleTitle",
+//       },
+//       {
+//         type: "input",
+//         message: "Enter Role Salary:",
+//         name: "roleSalary",
+//       },
+//     ])
+//     .then((data) => {
+//       connection.query(
+//         "INSERT INTO roles SET ?",
+//         {
+//           title: data.roleTitle,
+//           salary: data.roleSalary,
+//         },
+//         (err, res) => {
+//           if (err) throw err;
+//           console.log(`${res.affectedRows} new role added to the database.\n`);
+//         }
+//       );
+//     })
+//     .then(() => getUserOption());
+// }
 
-function insertEmployeeData() {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        message: "Enter Employee first name:",
-        name: "employeeFirstName",
-      },
-      {
-        type: "input",
-        message: "Enter Employee last name:",
-        name: "employeeLastName",
-      },
-      {
-        type: "list",
-        message: "Choose new employee's role:",
-        name: "employeeRole",
-        choices: roleChoices,
-      },
-      {
-        type: "list",
-        message: "Choose new employee's Manager:",
-        name: "employeeManager",
-        choices: managerChoices,
-      },
-    ])
-    .then((data) => {
-      connection.query(
-        "INSERT INTO employees SET ?",
-        {
-          first_name: data.employeeFirstName,
-          last_name: data.employeeLastName,
-          role_id: data.employeeRole,
-          manager_id: data.employeeManager,
-        },
-        (err, res) => {
-          if (err) throw err;
-          console.log(
-            `${res.affectedRows} new employee added to the database.\n`
-          );
-        }
-      );
-    });
-}
-
-function insertRoleData() {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        message: "Enter Role Title:",
-        name: "roleTitle",
-      },
-      {
-        type: "input",
-        message: "Enter Role Salary:",
-        name: "roleSalary",
-      },
-    ])
-    .then((data) => {
-      connection.query(
-        "INSERT INTO roles SET ?",
-        {
-          title: data.roleTitle,
-          salary: data.roleSalary,
-        },
-        (err, res) => {
-          if (err) throw err;
-          console.log(`${res.affectedRows} new role added to the database.\n`);
-        }
-      );
-    })
-    .then(() => getUserOption());
-}
-
-function insertDepartment() {
-  inquirer
-    .prompt({
-      type: "input",
-      message: "Enter new Department:",
-      name: "department",
-    })
-    .then((data) => {
-      connection.query(
-        "INSERT INTO departments set ?",
-        {
-          department_name: data.department,
-        },
-        (err, res) => {
-          if (err) throw err;
-          console.log(`${res.affectedRows} new department to the database.\n`);
-        }
-      );
-    })
-    .then(() => getUserOption());
-}
+// function insertDepartment() {
+//   inquirer
+//     .prompt({
+//       type: "input",
+//       message: "Enter new Department:",
+//       name: "department",
+//     })
+//     .then((data) => {
+//       connection.query(
+//         "INSERT INTO departments set ?",
+//         {
+//           department_name: data.department,
+//         },
+//         (err, res) => {
+//           if (err) throw err;
+//           console.log(`${res.affectedRows} new department to the database.\n`);
+//         }
+//       );
+//     })
+//     .then(() => getUserOption());
+// }
