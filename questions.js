ceb6bb4b41c312a50e297c8ae2d632ec2186fb50
Jonathan Rein <jrein1296@gmail.com>
2021-05-05 21:18:58 -0400

    In insertEmployeeData, INSERT INTO employees is running to early.

--- questions.js
+++ questions.js
@@ -1,64 +1,45 @@
-module.exports = {
-  userOptions: {
-    type: "list",
-    name: "userOption",
-    choices: [
-      "View all Employees",
-      "View all Employees by Department",
-      "View all employees by Manager",
-      "Add new Employee",
-      "Remove Employee",
-      "Update Employee Role",
-      "Update Employee Manager",
-      "Add new Role",
-      "Add new Department",
-      "Quit",
-    ],
-  },
 
-  addEmployee: [
-    {
-      type: "input",
-      message: "Enter Employee first name:",
-      name: "employeeFirstName",
-    },
-    {
-      type: "input",
-      message: "Enter Employee last name:",
-      name: "employeeLastName",
-    },
-    {
-      type: "list",
-      message: "Choose new employee's role:",
-      name: "employeeRole",
-      choices: []
-    },
-    {
-      type: "list",
-      message: "Choose new employee's Manager:",
-      name: "employeeManager",
-      choices: []
-    },
-  ],
 
-  addRole: [
-    {
-      type: "input",
-      message: "Enter Role Title:",
-      name: "roleTitle",
-    },
-    {
-      type: "input",
-      message: "Enter Role Salary:",
-      name: "roleSalary",
-    },
-  ],
+// module.exports = {
+//   userOptions: {
+//     type: "list",
+//     name: "userOption",
+//     choices: [
+//       "View all Employees",
+//       "View all Employees by Department",
+//       "View all employees by Manager",
+//       "Add new Employee",
+//       "Remove Employee",
+//       "Update Employee Role",
+//       "Update Employee Manager",
+//       "Add new Role",
+//       "Add new Department",
+//       "Quit",
+//     ],
+//   },
 
-  addDepartment: [
-    {
-      type: "input",
-      message: "Enter new Department:",
-      name: "department",
-    },
-  ],
-};
+
+
+  // addRole: [
+  //   {
+  //     type: "input",
+  //     message: "Enter Role Title:",
+  //     name: "roleTitle",
+  //   },
+  //   {
+  //     type: "input",
+  //     message: "Enter Role Salary:",
+  //     name: "roleSalary",
+  //   },
+  // ],
+
+//   addDepartment: [
+//     {
+//       type: "input",
+//       message: "Enter new Department:",
+//       name: "department",
+//     },
+//   ],
+// };
+
+
